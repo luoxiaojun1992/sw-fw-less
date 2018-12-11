ef9b35b0a398dc703cf5ae74273015a6683a2d50
luoxiaojun <luoxiaojun@dodoca.com>
2018-12-11 11:01:57 +0800

    add comment

--- app/components/Helper.php
+++ app/components/Helper.php
@@ -4,6 +4,12 @@ namespace App\components;
 
 class Helper
 {
+    /**
+     * @param $arr
+     * @param $key
+     * @param null $default
+     * @return null
+     */
     public static function arrGet($arr, $key, $default = null)
     {
         return isset($arr[$key]) ? $arr[$key] : $default;
