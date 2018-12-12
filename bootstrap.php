b6074158b8f7a5bfe09eb42a71210a757e1bd52b
luoxiaojun <luoxiaojun@dodoca.com>
2018-12-12 10:01:28 +0800

    bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+\Swoole\Runtime::enableCoroutine();
+
+//Dot Env
+(new Dotenv\Dotenv(__DIR__))->load();
+
+//Init Config
+\App\components\Config::init(require_once __DIR__ . '/config/app.php');
+
+//Route Config
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $routerConfig = \App\components\Config::get('router');
+    foreach ($routerConfig as $router) {
+        $r->addRoute($router[0], $router[1], $router[2]);
+    }
+});
--- start.php
+++ start.php
@@ -1,22 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
-
-\Swoole\Runtime::enableCoroutine();
-
-//Dot Env
-(new Dotenv\Dotenv(__DIR__))->load();
-
-//Init Config
-\App\components\Config::init(require_once __DIR__ . '/config/app.php');
-
-//Route Config
-$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
-    $routerConfig = \App\components\Config::get('router');
-    foreach ($routerConfig as $router) {
-        $r->addRoute($router[0], $router[1], $router[2]);
-    }
-});
+require_once __DIR__ . '/bootstrap.php';
 
 $http = new \Swoole\Http\Server(\App\components\Config::get('server.host'), \App\components\Config::get('server.port'));
 
