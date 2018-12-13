70c99013c37fd804ff5872352a9d4ec174f52d45
luoxiaojun <luoxiaojun@dodoca.com>
2018-12-13 23:38:03 +0800

    validator

--- app/components/Validator.php
+++ app/components/Validator.php
@@ -2,6 +2,8 @@
 
 namespace App\components;
 
+use App\components\utils\IDCard;
+
 class Validator
 {
     /**
@@ -12,5 +14,25 @@ class Validator
     public static function string($value, $context)
     {
         return is_string($value);
+    }
+
+    /**
+     * @param $value
+     * @param $context
+     * @return false|int
+     */
+    public static function mobile($value, $context)
+    {
+        return boolval(preg_match('/^1\d{10}$/', $value));
+    }
+
+    /**
+     * @param $value
+     * @param $context
+     * @return bool
+     */
+    public static function idCard($value, $context)
+    {
+        return IDCard::validateIDCard($value);
     }
 }
