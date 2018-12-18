7c993b736b236d9666239b00561bd3f400c7de33
luoxiaojun <luoxiaojun@dodoca.com>
2018-12-18 20:01:19 +0800

    docker

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 \Swoole\Runtime::enableCoroutine();
 
 //Dot Env
-(new Dotenv\Dotenv(__DIR__))->load();
+if (file_exists(__DIR__  . '/.env')) {
+    (new Dotenv\Dotenv(__DIR__))->load();
+}
 
 //Init Config
 \App\components\Config::init(require_once __DIR__ . '/config/app.php');
