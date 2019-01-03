6635478d466504b8a62d4f8e2fe3505ced76a460
luoxiaojun <luoxiaojun@dodoca.com>
2019-01-03 19:22:52 +0800

    monitor bugfix

--- app/services/internals/MonitorService.php
+++ app/services/internals/MonitorService.php
@@ -17,15 +17,15 @@ class MonitorService extends BaseService
     {
         if (extension_loaded('swoole')) {
             return Response::json([
-                'redis' => (Config::get('redis.pool_change_event') && Config::get('redis.report_pool_change')) ?
+                'redis' => Config::get('redis.pool_change_event') && Config::get('redis.report_pool_change') ?
                     Counter::get('monitor:pool:redis') : RedisPool::countPool(),
-                'mysql' => (Config::get('mysql.pool_change_event') && Config::get('mysql.report_pool_change')) ?
+                'mysql' => Config::get('mysql.pool_change_event') && Config::get('mysql.report_pool_change') ?
                     Counter::get('monitor:pool:mysql') : MysqlPool::countPool(),
                 'log' => [
                     'pool' => Log::countPool(),
                     'record_buffer' => Log::countRecordBuffer(),
                 ],
-                'amqp' => (Config::get('amqp.pool_change_event') && Config::get('amqp.report_pool_change')) ?
+                'amqp' => Config::get('amqp.pool_change_event') && Config::get('amqp.report_pool_change') ?
                     Counter::get('monitor:pool:amqp') : AMQPConnectionPool::countPool(),
             ]);
         } else {
