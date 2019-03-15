85602dc3b9cd5bcd6b055c5f25637b40746cde46
luoxiaojun <luoxiaojun@dodoca.com>
2019-03-15 14:46:43 +0800

    providers

--- config/app.php
+++ config/app.php
@@ -282,6 +282,18 @@ return [
 
     //Providers
     'providers' => [
+        //App Providers
+        \App\components\datetime\DatetimeProvider::class,
         \App\components\utils\swoole\counter\CounterProvider::class,
+        \App\components\event\EventProvider::class,
+
+        //Request Providers
+        \App\components\log\LogProvider::class,
+        \App\components\redis\RedisProvider::class,
+        \App\components\mysql\MysqlProvider::class,
+        \App\components\es\EsProvider::class,
+        \App\components\storage\StorageProvider::class,
+        \App\components\amqp\AmqpProvider::class,
+        \App\components\hbase\HbaseProvider::class,
     ],
 ];
