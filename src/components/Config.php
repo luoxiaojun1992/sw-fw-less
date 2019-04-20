3ebb0f373f093ad73de41c3a81d31528a4e739f1
luoxiaojun <luoxiaojun@dodoca.com>
2019-04-20 19:06:54 +0800

    update config component

--- src/components/Config.php
+++ src/components/Config.php
@@ -11,7 +11,25 @@ class Config
      */
     public static function init($config)
     {
-        self::$config = $config;
+        self::$config = self::mergeSpecConfigs($config);
+    }
+
+    private static function mergeSpecConfigs($appConfig)
+    {
+        $fd = opendir(APP_BASE_PATH . 'config');
+        while($file = readdir($fd)) {
+            if (!in_array($file, ['.', '..', 'app.php'])) {
+                $configName = substr($file, 0, -4);
+                if (isset($appConfig[$configName])) {
+                    $appConfig[$configName] = array_merge($appConfig[$configName], require APP_BASE_PATH . 'config/' . $file);
+                } else {
+                    $appConfig[$configName] = require APP_BASE_PATH . 'config/' . $file;
+                }
+            }
+        }
+        closedir($fd);
+
+        return $appConfig;
     }
 
     /**
