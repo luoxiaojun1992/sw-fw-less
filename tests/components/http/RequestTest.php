1f012b46315286b018b90a6e26203fbdce5fb722
luoxiaojun <luoxiaojun@dodoca.com>
2019-06-24 16:30:49 +0800

    add request unit tests

--- tests/components/http/RequestTest.php
+++ tests/components/http/RequestTest.php
@@ -31,11 +31,9 @@ class RequestTest extends \PHPUnit\Framework\TestCase
     public function testRoute()
     {
         $swfRequest = $this->createSwfRequest();
-
         $this->assertEquals('/test', $swfRequest->setRoute('/test')->getRoute());
 
         $swfRequest = $this->createSwfRequest();
-
         $this->assertEquals(null, $swfRequest->getRoute());
     }
 
@@ -46,19 +44,16 @@ class RequestTest extends \PHPUnit\Framework\TestCase
     {
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->{$paramType} = [];
         $swRequest->{$paramType}['foo'] = 'bar';
         $swRequest->{$paramType}['FOO'] = 'BAR';
-
         $this->assertEquals('bar', $swfRequest->{$paramType}('foo'));
         $this->assertEquals('BAR', $swfRequest->{$paramType}('FOO'));
 
         $swfRequest = $this->createSwfRequest();
-
         $this->assertEquals(null, $swfRequest->{$paramType}('foo'));
 
         $swfRequest = $this->createSwfRequest();
-
         $this->assertEquals('default', $swfRequest->{$paramType}('foo', 'default'));
     }
 
@@ -76,15 +71,13 @@ class RequestTest extends \PHPUnit\Framework\TestCase
     {
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->files = [];
         $swRequest->files['foo'] = 'bar';
         $swRequest->files['FOO'] = 'BAR';
-
         $this->assertEquals('bar', $swfRequest->file('foo'));
         $this->assertEquals('BAR', $swfRequest->file('FOO'));
 
         $swfRequest = $this->createSwfRequest();
-
         $this->assertEquals(null, $swfRequest->file('foo'));
     }
 
@@ -92,28 +85,24 @@ class RequestTest extends \PHPUnit\Framework\TestCase
     {
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->post = [];
         $swRequest->post['foo'] = 'bar';
-
         $this->assertEquals('bar', $swfRequest->param('foo'));
 
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->get = [];
         $swRequest->get['foo'] = 'bar';
-
         $this->assertEquals('bar', $swfRequest->param('foo'));
 
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->files = [];
         $swRequest->files['foo'] = 'bar';
-
         $this->assertEquals('bar', $swfRequest->param('foo'));
 
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
         $this->assertEquals('bar', $swfRequest->param('foo', 'bar'));
     }
 
@@ -122,8 +111,11 @@ class RequestTest extends \PHPUnit\Framework\TestCase
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
 
+        $swRequest->get = [];
         $swRequest->get['param1'] = 'value1';
+        $swRequest->post = [];
         $swRequest->post['param2'] = 'value2';
+        $swRequest->files = [];
         $swRequest->files['param3'] = 'value3';
 
         $this->assertEquals(['param1' => 'value1', 'param2' => 'value2', 'param3' => 'value3'], $swfRequest->all());
@@ -133,7 +125,7 @@ class RequestTest extends \PHPUnit\Framework\TestCase
     {
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->header = [];
         $swRequest->header['content-type'] = 'application/json';
 
         $this->assertMultiEquals('application/json', [
@@ -149,7 +141,7 @@ class RequestTest extends \PHPUnit\Framework\TestCase
     {
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->header = [];
         $swRequest->header['content-type'] = 'application/json';
 
         $this->assertMultiEquals(true, [
@@ -164,45 +156,41 @@ class RequestTest extends \PHPUnit\Framework\TestCase
     {
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->header = [];
         $swRequest->header['x-real-ip'] = '172.17.0.0';
-
         $this->assertEquals('172.17.0.0', $swfRequest->realIp());
 
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->header = [];
         $swRequest->header['x-forwarded-for'] = '172.17.0.0';
-
         $this->assertEquals('172.17.0.0', $swfRequest->realIp());
 
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->server = [];
         $swRequest->server['remote_addr'] = '172.17.0.0';
-
         $this->assertEquals('172.17.0.0', $swfRequest->realIp());
 
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->header = [];
         $swRequest->header['x-real-ip'] = '172.17.0.0';
         $swRequest->header['x-forwarded-for'] = '10.0.0.0';
+        $swRequest->server = [];
         $swRequest->server['remote_addr'] = '10.0.0.0';
-
         $this->assertEquals('172.17.0.0', $swfRequest->realIp());
 
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->header = [];
         $swRequest->header['x-forwarded-for'] = '172.17.0.0';
+        $swRequest->server = [];
         $swRequest->server['remote_addr'] = '10.0.0.0';
-
         $this->assertEquals('172.17.0.0', $swfRequest->realIp());
 
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
         $this->assertEquals(null, $swfRequest->realIp());
     }
 
@@ -210,7 +198,7 @@ class RequestTest extends \PHPUnit\Framework\TestCase
     {
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->server = [];
         $swRequest->server['remote_addr'] = '10.0.0.0';
 
         $this->assertMultiEquals('10.0.0.0', [
@@ -226,7 +214,7 @@ class RequestTest extends \PHPUnit\Framework\TestCase
     {
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->server = [];
         $swRequest->server['remote_addr'] = '10.0.0.0';
 
         $this->assertMultiEquals(true, [
@@ -241,14 +229,12 @@ class RequestTest extends \PHPUnit\Framework\TestCase
     {
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->server = [];
         $swRequest->server['request_method'] = 'GET';
-
         $this->assertEquals('GET', $swfRequest->method());
 
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
         $this->assertEquals(null, $swfRequest->method());
     }
 
@@ -256,14 +242,12 @@ class RequestTest extends \PHPUnit\Framework\TestCase
     {
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->server = [];
         $swRequest->server['request_uri'] = '/foo/bar';
-
         $this->assertEquals('/foo/bar', $swfRequest->uri());
 
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
         $this->assertEquals(null, $swfRequest->uri());
     }
 
@@ -271,14 +255,12 @@ class RequestTest extends \PHPUnit\Framework\TestCase
     {
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
+        $swRequest->server = [];
         $swRequest->server['query_string'] = 'foo=bar';
-
         $this->assertEquals('foo=bar', $swfRequest->queryString());
 
         $swRequest = $this->createSwRequest();
         $swfRequest = $this->createSwfRequest($swRequest);
-
         $this->assertEquals(null, $swfRequest->queryString());
     }
 
@@ -293,5 +275,47 @@ class RequestTest extends \PHPUnit\Framework\TestCase
         $swfRequest = $this->createSwfRequest($swRequest);
 
         $this->assertEquals('test', $swfRequest->body());
+    }
+
+    public function testIsGrpc()
+    {
+        $swRequest = $this->createSwRequest();
+        $swfRequest = $this->createSwfRequest($swRequest);
+        $swRequest->header = [];
+        $swRequest->header['content-type'] = 'application/grpc';
+        $this->assertTrue($swfRequest->isGrpc());
+
+        $swRequest = $this->createSwRequest();
+        $swfRequest = $this->createSwfRequest($swRequest);
+        $swRequest->header = [];
+        $swRequest->header['content-type'] = 'application/grpc+json';
+        $this->assertTrue($swfRequest->isGrpc());
+
+        $swRequest = $this->createSwRequest();
+        $swfRequest = $this->createSwfRequest($swRequest);
+        $swRequest->header = [];
+        $swRequest->header['content-type'] = 'application/json';
+        $this->assertFalse($swfRequest->isGrpc());
+    }
+
+    public function testIsGrpcJson()
+    {
+        $swRequest = $this->createSwRequest();
+        $swfRequest = $this->createSwfRequest($swRequest);
+        $swRequest->header = [];
+        $swRequest->header['content-type'] = 'application/grpc';
+        $this->assertFalse($swfRequest->isGrpcJson());
+
+        $swRequest = $this->createSwRequest();
+        $swfRequest = $this->createSwfRequest($swRequest);
+        $swRequest->header = [];
+        $swRequest->header['content-type'] = 'application/grpc+json';
+        $this->assertTrue($swfRequest->isGrpcJson());
+
+        $swRequest = $this->createSwRequest();
+        $swfRequest = $this->createSwfRequest($swRequest);
+        $swRequest->header = [];
+        $swRequest->header['content-type'] = 'application/json';
+        $this->assertFalse($swfRequest->isGrpcJson());
     }
 }
