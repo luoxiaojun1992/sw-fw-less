c924ad5c782e0276a972662385d5f48d400f5343
luoxiaojun <luoxiaojun@dodoca.com>
2020-04-17 23:31:34 +0800

    add bit int arr

--- src/components/utils/bitmap/bitarray/BitIntArr.php
+++ src/components/utils/bitmap/bitarray/BitIntArr.php
@@ -6,31 +6,54 @@ class BitIntArr
 {
     protected $slots;
 
+    //todo unit test
+
     protected function slotStorage()
     {
         return intval(log(PHP_INT_MAX - 1, 2));
     }
 
-    public function set($number)
+    protected function getSlotIndex($number)
+    {
+        return ceil($number / $this->slotStorage());
+    }
+
+    protected function getFractionalAmount($number)
     {
         $slotStorage = $this->slotStorage();
-
-        $slotIndex = ceil($number / $slotStorage);
-        if (!isset($this->slots[$slotIndex])) {
-            $this->slots[$slotIndex] = 0;
-        }
 
         $fractionalAmount = $number % $slotStorage;
         if ($fractionalAmount == 0) {
             $fractionalAmount = $slotStorage;
         }
 
-        $bitMap = 1 << ($fractionalAmount - 1);
-        $this->slots[$slotIndex] = $this->slots[$slotIndex] & $bitMap;
+        return $fractionalAmount;
+    }
+
+    public function set($number)
+    {
+        $slotIndex = $this->getSlotIndex($number);
+        if (!isset($this->slots[$slotIndex])) {
+            $this->slots[$slotIndex] = 0;
+        }
+
+        $fractionalAmount = $this->getFractionalAmount($number);
+
+        $bitNumber = 1 << ($fractionalAmount - 1);
+        $this->slots[$slotIndex] = $this->slots[$slotIndex] | $bitNumber;
     }
 
     public function has($number)
     {
+        $slotIndex = $this->getSlotIndex($number);
+        if (!isset($this->slots[$slotIndex])) {
+            return false;
+        }
 
+        $fractionalAmount = $this->getFractionalAmount($number);
+
+        $bitNumber = 1 << ($fractionalAmount - 1);
+
+        return ($this->slots[$slotIndex] & $bitNumber) === $bitNumber;
     }
 }
