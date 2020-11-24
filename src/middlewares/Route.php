c2cef1fda44acf21a307e56c8f97ded64cf251a1
luoxiaojun <luoxiaojun1992@sina.cn>
2020-11-24 18:06:08 +0800

    performance optimization

--- src/middlewares/Route.php
+++ src/middlewares/Route.php
@@ -14,6 +14,8 @@ class Route extends AbstractMiddleware
 {
     static $cachedRouteInfo = [];
 
+    static $cachedRouteInfoCount = 0;
+
     use Parser;
 
     private function getRequestHandler(Request $appRequest, $routeInfo)
@@ -83,8 +85,10 @@ class Route extends AbstractMiddleware
                 self::$cachedRouteInfo[$cacheKey] = $routeInfo = $httpRouteDispatcher->dispatch(
                     $request->method(), $requestUri
                 );
-                if (count(self::$cachedRouteInfo) > 100) {
+                ++self::$cachedRouteInfoCount;
+                if (self::$cachedRouteInfoCount > 100) {
                     self::$cachedRouteInfo = array_slice(self::$cachedRouteInfo, 0, 100, true);
+                    self::$cachedRouteInfoCount = 100;
                 }
             }
             return $routeInfo;
