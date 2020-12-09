a12cc8885c9d9a9b0ddf11d4cd975e415098eb82
luoxiaojun <luoxiaojun1992@sina.cn>
2020-12-09 18:24:48 +0800

    response test

--- tests/components/http/ResponseTest.php
+++ tests/components/http/ResponseTest.php
@@ -2,8 +2,6 @@
 
 class ResponseTest extends \PHPUnit\Framework\TestCase
 {
-    //TODO
-
     /**
      * @return \SwFwLess\components\http\Response
      */
@@ -12,10 +10,63 @@ class ResponseTest extends \PHPUnit\Framework\TestCase
         return new \SwFwLess\components\http\Response();
     }
 
+    public function testContent()
+    {
+        $responseContent = 'pong';
+        $response = $this->createResponse();
+        $response->setContent($responseContent);
+        $this->assertEquals(
+            $responseContent,
+            $response->getContent()
+        );
+
+        $arr = ['foo' => 'bar'];
+        $jsonResponse = \SwFwLess\components\http\Response::json($arr);
+        $this->assertJsonStringEqualsJsonString(
+            json_encode($arr),
+            $jsonResponse->getContent()
+        );
+
+        $rawResponseContent = 'bar';
+        $rawResponse = \SwFwLess\components\http\Response::output($rawResponseContent);
+        $this->assertEquals(
+            $rawResponseContent,
+            $rawResponse->getContent()
+        );
+    }
+
     public function testStatus()
     {
         $response = $this->createResponse();
         $response->setStatus(200);
         $this->assertEquals(200, $response->getStatus());
+    }
+
+    public function testHeaders()
+    {
+        $response = $this->createResponse();
+        $response->setHeaders([
+            'content-type' => 'application/json'
+        ]);
+        $headers = $response->getHeaders();
+        $this->assertEquals(
+            'application/json',
+            $headers['content-type']
+        );
+
+        $response = $this->createResponse();
+        $response->setHeaders([
+            'content-type' => 'application/json',
+            'cache-control' => 'no-cache',
+        ]);
+        $headers = $response->getHeaders();
+        $this->assertEquals(
+            'application/json',
+            $headers['content-type']
+        );
+        $this->assertEquals(
+            'no-cache',
+            $headers['cache-control']
+        );
     }
 }
