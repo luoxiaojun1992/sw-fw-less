19cc56f267ad20f5d036091a7c97a0d07095671c
luoxiaojun <luoxiaojun1992@sina.cn>
2020-12-10 13:05:52 +0800

    grpc serializer test

--- tests/components/grpc/SerializerTest.php
+++ tests/components/grpc/SerializerTest.php
@@ -3,4 +3,19 @@
 class SerializerTest extends \PHPUnit\Framework\TestCase
 {
     //TODO
+
+    public function testUnpack()
+    {
+        //TODO
+    }
+
+    public function testUnpackHeader()
+    {
+        //TODO
+    }
+
+    public function testPack()
+    {
+        //TODO
+    }
 }
