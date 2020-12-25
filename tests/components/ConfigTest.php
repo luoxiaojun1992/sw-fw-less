29a08efb55c29e15c85ee257f4d87fce73b10f14
luoxiaojun <luoxiaojun1992@sina.cn>
2020-12-25 17:48:31 +0800

    test: config test

--- tests/components/ConfigTest.php
+++ tests/components/ConfigTest.php
@@ -32,5 +32,167 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
             'bar',
             \SwFwLess\components\Config::get('foo')
         );
+
+        \SwFwLess\components\Config::set('foo', 'bar1');
+        $this->assertEquals(
+            'bar1',
+            \SwFwLess\components\Config::get('foo')
+        );
+
+        \SwFwLess\components\Config::set('foo2', 'bar2');
+        $this->assertEquals(
+            'bar2',
+            \SwFwLess\components\Config::get('foo2')
+        );
+    }
+
+    public function testSetAndForget()
+    {
+        $this->mockSwooleScheduler();
+
+        \SwFwLess\components\Config::initByArr([
+            'foo' => 'bar'
+        ]);
+
+        $this->assertTrue(
+            \SwFwLess\components\Config::has('foo')
+        );
+
+        \SwFwLess\components\Config::forget('foo');
+        $this->assertFalse(
+            \SwFwLess\components\Config::has('foo')
+        );
+
+        \SwFwLess\components\Config::set('foo', 'bar1');
+        $this->assertTrue(
+            \SwFwLess\components\Config::has('foo')
+        );
+
+        \SwFwLess\components\Config::forget('foo');
+        $this->assertFalse(
+            \SwFwLess\components\Config::has('foo')
+        );
+
+        \SwFwLess\components\Config::set('foo2', 'bar2');
+        $this->assertTrue(
+            \SwFwLess\components\Config::has('foo2')
+        );
+
+        \SwFwLess\components\Config::forget('foo2');
+        $this->assertFalse(
+            \SwFwLess\components\Config::has('foo2')
+        );
+    }
+
+    public function testSetAndHas()
+    {
+        $this->mockSwooleScheduler();
+
+        \SwFwLess\components\Config::initByArr([
+            'foo' => 'bar'
+        ]);
+
+        $this->assertTrue(
+            \SwFwLess\components\Config::has('foo')
+        );
+
+        \SwFwLess\components\Config::set('foo', 'bar1');
+        $this->assertTrue(
+            \SwFwLess\components\Config::has('foo')
+        );
+
+        \SwFwLess\components\Config::set('foo2', 'bar2');
+        $this->assertTrue(
+            \SwFwLess\components\Config::has('foo2')
+        );
+    }
+
+    public function testSetGetAndForget()
+    {
+        $this->mockSwooleScheduler();
+
+        \SwFwLess\components\Config::initByArr([
+            'foo' => 'bar'
+        ]);
+
+        $this->assertTrue(
+            \SwFwLess\components\Config::has('foo')
+        );
+
+        \SwFwLess\components\Config::forget('foo');
+        $this->assertFalse(
+            \SwFwLess\components\Config::has('foo')
+        );
+        $this->assertNull(
+            \SwFwLess\components\Config::get('foo')
+        );
+        $this->assertEquals(
+            'defaultBar',
+            \SwFwLess\components\Config::get('foo', 'defaultBar')
+        );
+
+        \SwFwLess\components\Config::set('foo', 'bar1');
+        $this->assertTrue(
+            \SwFwLess\components\Config::has('foo')
+        );
+        $this->assertEquals(
+            'bar1',
+            \SwFwLess\components\Config::get('foo')
+        );
+
+        \SwFwLess\components\Config::forget('foo');
+        $this->assertFalse(
+            \SwFwLess\components\Config::has('foo')
+        );
+        $this->assertNull(
+            \SwFwLess\components\Config::get('foo')
+        );
+        $this->assertEquals(
+            'defaultBar1',
+            \SwFwLess\components\Config::get('foo', 'defaultBar1')
+        );
+
+        \SwFwLess\components\Config::set('foo2', 'bar2');
+        $this->assertTrue(
+            \SwFwLess\components\Config::has('foo2')
+        );
+
+        \SwFwLess\components\Config::forget('foo2');
+        $this->assertFalse(
+            \SwFwLess\components\Config::has('foo2')
+        );
+        $this->assertNull(
+            \SwFwLess\components\Config::get('foo2')
+        );
+        $this->assertEquals(
+            'defaultBar2',
+            \SwFwLess\components\Config::get('foo', 'defaultBar2')
+        );
+    }
+
+    public function testGetDefault()
+    {
+        $this->mockSwooleScheduler();
+
+        \SwFwLess\components\Config::initByArr([
+            'foo' => 'bar'
+        ]);
+
+        $this->assertEquals(
+            'bar3',
+            \SwFwLess\components\Config::get('foo3', 'bar3')
+        );
+
+        \SwFwLess\components\Config::set('foo', 'bar1');
+        $this->assertEquals(
+            'bar_5',
+            \SwFwLess\components\Config::get('foo5', 'bar_5')
+        );
+
+        \SwFwLess\components\Config::set('foo2', 'bar2');
+        $this->assertEquals(
+            'bar_6',
+            \SwFwLess\components\Config::get('foo6', 'bar_6')
+        );
     }
 }
