34df035084fb887bf2833281f5116a6b4f84127d
luoxiaojun <luoxiaojun1992@sina.cn>
2021-01-12 18:21:18 +0800

    test: str util test

--- tests/components/utils/StrTest.php
+++ tests/components/utils/StrTest.php
@@ -4,8 +4,28 @@ class StrTest extends \PHPUnit\Framework\TestCase
 {
     public function testContains()
     {
-        //TODO
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('abc', ''));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('abc', 'a'));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('abc', 'b'));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('abc', 'c'));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('abc', 'ab'));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('abc', 'bc'));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('abc', 'abc'));
+        $this->assertFalse(\SwFwLess\components\utils\Str::contains('abc', 'd'));
+        $this->assertFalse(\SwFwLess\components\utils\Str::contains('abc', 'ac'));
 
-        $this->assertTrue(true);
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('你好，世界', ''));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('你好，世界', '你'));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('你好，世界', '好'));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('你好，世界', '世'));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('你好，世界', '界'));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('你好，世界', '，'));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('你好，世界', '你好'));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('你好，世界', '世界'));
+        $this->assertFalse(\SwFwLess\components\utils\Str::contains('你好，世界', '！'));
+        $this->assertFalse(\SwFwLess\components\utils\Str::contains('你好，世界', '好世界'));
+
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('你好，世界 Hello World', '世界'));
+        $this->assertTrue(\SwFwLess\components\utils\Str::contains('你好，世界 Hello World', '世界 Hello'));
     }
 }
