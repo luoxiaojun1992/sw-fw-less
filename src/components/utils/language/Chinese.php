3f7ea9e27c41499646a0662de592498a4c7bafa1
luoxiaojun <luoxiaojun1992@sina.cn>
2021-01-12 11:24:04 +0800

    chinese language util

--- src/components/utils/language/Chinese.php
+++ src/components/utils/language/Chinese.php
@@ -7,6 +7,6 @@ class Chinese
     public static function containsChinese($str)
     {
         //TODO
-        return false;
+        return boolval(preg_match('/([\x{4E00}-\x{9FA5}]|[\x{3400}-\x{4DB5}])/u', $str));
     }
 }
