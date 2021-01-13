40665df9c38810573bbcd138c8a47fb2bd4608fa
luoxiaojun <luoxiaojun1992@sina.cn>
2021-01-13 14:49:29 +0800

    test: ip util test

--- tests/components/utils/IpTest.php
+++ tests/components/utils/IpTest.php
@@ -8,5 +8,30 @@ class IpTest extends \PHPUnit\Framework\TestCase
             '172.19.10.1',
             '172.19.0.0/16'
         ));
+
+        $this->assertTrue(\SwFwLess\components\utils\Ip::checkIp(
+            '172.19.10.1',
+            '192.168.0.1/0'
+        ));
+
+        $this->assertTrue(\SwFwLess\components\utils\Ip::checkIp(
+            '172.19.10.1',
+            '172.19.10.1'
+        ));
+
+        $this->assertFalse(\SwFwLess\components\utils\Ip::checkIp(
+            '172.20.10.1',
+            '172.19.0.0/16'
+        ));
+
+        $this->assertFalse(\SwFwLess\components\utils\Ip::checkIp(
+            '172.19.10.1',
+            '192.168.0.1.1/0'
+        ));
+
+        $this->assertFalse(\SwFwLess\components\utils\Ip::checkIp(
+            '172.19.10.2',
+            '172.19.10.1'
+        ));
     }
 }
