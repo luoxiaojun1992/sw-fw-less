ac8d089457746a1664533604544c13ae6025d3dc
luoxiaojun <luoxiaojun1992@sina.cn>
2021-02-23 16:43:24 +0800

    context

---
+++ tests/components/container/ContainerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class ContainerTest extends \PHPUnit\Framework\TestCase
+{
+    public function testDataOperation()
+    {
+        //TODO
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/components/context/ContextTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class ContextTest extends \PHPUnit\Framework\TestCase
+{
+    public function testDataOperation()
+    {
+        //TODO
+        $this->assertTrue(true);
+    }
+}
