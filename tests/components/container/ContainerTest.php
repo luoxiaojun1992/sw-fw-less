18421903b2b0d45016fb1019215bcb9efc8aae9b
luoxiaojun <luoxiaojun1992@sina.cn>
2021-03-02 13:49:58 +0800

    test: container test

--- tests/components/container/ContainerTest.php
+++ tests/components/container/ContainerTest.php
@@ -4,7 +4,13 @@ class ContainerTest extends \PHPUnit\Framework\TestCase
 {
     public function testDataOperation()
     {
-        //TODO
-        $this->assertTrue(true);
+        $container = \SwFwLess\components\container\Container::createByData(['id' => 1]);
+
+        $this->assertEquals(1, $container->get('id'));
+
+        $container->set('foo', 'bar');
+
+        $this->assertEquals('bar', $container->get('foo'));
+        $this->assertEquals(1, $container->get('id'));
     }
 }
