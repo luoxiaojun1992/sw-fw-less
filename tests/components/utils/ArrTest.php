35057a841d2f7efd3db266c399803d672aa4aa95
luoxiaojun <luoxiaojun1992@sina.cn>
2021-03-16 13:17:03 +0800

    array mapping test

--- tests/components/utils/ArrTest.php
+++ tests/components/utils/ArrTest.php
@@ -14,4 +14,22 @@ class ArrTest extends \PHPUnit\Framework\TestCase
             $intArr === ['foo' => 1, 'bar' => 2]
         );
     }
+
+    public function testMapping()
+    {
+        $origArr = [
+            ['col1' => 'row1 col1 value', 'col2' => 'row1 col2 value'],
+            ['col1' => 'row2 col1 value', 'col2' => 'row2 col2 value'],
+        ];
+
+        $this->assertEquals([
+            'row1 col1 value' => 'row1 col2 value',
+            'row2 col1 value' => 'row2 col2 value',
+        ], \SwFwLess\components\utils\Arr::mapping($origArr, 'col1', 'col2'));
+
+        $this->assertEquals([
+            'row1 col1 value' => ['col1' => 'row1 col1 value', 'col2' => 'row1 col2 value'],
+            'row2 col1 value' => ['col1' => 'row2 col1 value', 'col2' => 'row2 col2 value'],
+        ], \SwFwLess\components\utils\Arr::mapping($origArr, 'col1'));
+    }
 }
