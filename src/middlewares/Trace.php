03510b19e7650712d9a088e0de817f5ce8308a96
luoxiaojun <luoxiaojun1992@sina.cn>
2021-06-02 17:06:56 +0800

    request trace info

--- src/middlewares/Trace.php
+++ src/middlewares/Trace.php
@@ -2,37 +2,78 @@
 
 namespace SwFwLess\middlewares;
 
+use SwFwLess\components\http\Code;
 use SwFwLess\components\http\Request;
 use SwFwLess\components\http\Response;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 class Trace extends AbstractMiddleware
 {
-    protected function requestInfo(Request $request, Response $response, $requestDuration)
+    protected function requestInfo(
+        Request $request, ?Response $response, $requestDuration, $memoryUsage
+    )
     {
         $output = new ConsoleOutput();
         $requestMethod = $request->method();
         $uri = $request->uri();
-        $httpCode = $response->getStatus();
+        $httpCode = $response ? ($response->getStatus()) : (Code::STATUS_INTERNAL_SERVER_ERROR);
+        if ($httpCode < 400) {
+            $traceLevel = 'info';
+        } elseif ($httpCode < 500) {
+            $traceLevel = 'comment';
+        } else {
+            $traceLevel = 'error';
+        }
+
         if ($requestDuration >= 1) {
             $formattedReqDuration = round($requestDuration, 2);
-            $requestDurationText = $formattedReqDuration . ' sec';
+            $requestDurationText = ((string)$formattedReqDuration) . ' sec';
         } elseif ($requestDuration >= 0.001) {
             $formattedReqDuration = round($requestDuration * 1000, 2);
-            $requestDurationText = $formattedReqDuration . ' ms';
+            $requestDurationText = ((string)$formattedReqDuration) . ' ms';
         } else {
             $formattedReqDuration = round($requestDuration * 1000000, 2);
-            $requestDurationText = $formattedReqDuration . ' us';
+            $requestDurationText = ((string)$formattedReqDuration) . ' us';
         }
-        $output->writeln("<info>{$httpCode}    {$requestMethod} {$uri}    {$requestDurationText}</info>");
+
+        if ($memoryUsage >= 1000000000) { //GB
+            $formattedMemUsage = round($memoryUsage / 1000000000, 2);
+            $memUsageText = ((string)$formattedMemUsage) . ' GB';
+        } elseif ($memoryUsage >= 1000000) { //MB
+            $formattedMemUsage = round($memoryUsage / 1000000, 2);
+            $memUsageText = ((string)$formattedMemUsage) . ' MB';
+        } elseif ($memoryUsage >= 1000) { //KB
+            $formattedMemUsage = round($memoryUsage / 1000, 2);
+            $memUsageText = ((string)$formattedMemUsage) . ' KB';
+        } else {
+            $memUsageText = ((string)$memoryUsage) . ' Bytes';
+        }
+
+        $output->writeln(
+            "<{$traceLevel}>{$httpCode}</{$traceLevel}>" .
+            "    " .
+            "<info>{$requestMethod} {$uri}" .
+            "    " .
+            "{$memUsageText}" .
+            "    " .
+            "{$requestDurationText}</info>"
+        );
     }
 
     public function handle(Request $request)
     {
+        $beforeReqMem = memory_get_usage();
         $requestTs = microtime(true);
-        $response = $this->next();
-        $requestDuration = microtime(true) - $requestTs;
-        $this->requestInfo($request, $response, $requestDuration);
+        $response = null;
+        try {
+            $response = $this->next();
+        } catch (\Throwable $e) {
+            throw $e;
+        } finally {
+            $requestDuration = microtime(true) - $requestTs;
+            $requestMemUsage = memory_get_usage() - $beforeReqMem;
+            $this->requestInfo($request, $response, $requestDuration, $requestMemUsage);
+        }
         return $response;
     }
 }
