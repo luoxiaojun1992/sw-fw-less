249dc8c66a7ec72acfc259dfcc92eb2098df6a7c
luoxiaojun <luoxiaojun1992@sina.cn>
2021-07-02 14:46:34 +0800

    test simd

--- tests/components/utils/math/MathTest.php
+++ tests/components/utils/math/MathTest.php
@@ -45,4 +45,82 @@ class MathTest extends \PHPUnit\Framework\TestCase
             $mathUtil->sum($testArr)
         );
     }
+
+    public function testVectorAdd()
+    {
+        $this->mockSwooleScheduler();
+
+        $mathUtil = \SwFwLess\components\utils\math\Math::create([
+            'pool_size' => 1,
+        ]);
+
+        $arrCount = 100000;
+
+        $testArr1 = range(1, $arrCount);
+        $testArr2 = range(1, $arrCount);
+        $sum = [];
+
+        $vector1 = $mathUtil->createCFloatNumbers(4);
+        $vector2 = $mathUtil->createCFloatNumbers(4);
+
+        for ($i = 0; $i < $arrCount; $i = $i + 4) {
+            $vector1[0] = $testArr1[$i];
+            $vector1[1] = $testArr1[$i + 1];
+            $vector1[2] = $testArr1[$i + 2];
+            $vector1[3] = $testArr1[$i + 3];
+
+            $vector2[0] = $testArr2[$i];
+            $vector2[1] = $testArr2[$i + 1];
+            $vector2[2] = $testArr2[$i + 2];
+            $vector2[3] = $testArr2[$i + 3];
+
+            $sumVector = $mathUtil->vectorAdd($vector1, $vector2, 4);
+            foreach ($sumVector as $elementSum) {
+                $sum[] = $elementSum;
+            }
+        }
+
+        foreach ($sum as $i => $value) {
+            $this->assertEquals(floatval(2 * ($i + 1)), $value);
+        }
+    }
+
+    public function testVectorSub()
+    {
+        $this->mockSwooleScheduler();
+
+        $mathUtil = \SwFwLess\components\utils\math\Math::create([
+            'pool_size' => 1,
+        ]);
+
+        $arrCount = 100000;
+
+        $testArr1 = range(1, $arrCount);
+        $testArr2 = range(1, $arrCount);
+        $diff = [];
+
+        $vector1 = $mathUtil->createCFloatNumbers(4);
+        $vector2 = $mathUtil->createCFloatNumbers(4);
+
+        for ($i = 0; $i < $arrCount; $i = $i + 4) {
+            $vector1[0] = $testArr1[$i];
+            $vector1[1] = $testArr1[$i + 1];
+            $vector1[2] = $testArr1[$i + 2];
+            $vector1[3] = $testArr1[$i + 3];
+
+            $vector2[0] = $testArr2[$i];
+            $vector2[1] = $testArr2[$i + 1];
+            $vector2[2] = $testArr2[$i + 2];
+            $vector2[3] = $testArr2[$i + 3];
+
+            $sumVector = $mathUtil->vectorSub($vector1, $vector2, 4);
+            foreach ($sumVector as $elementDiff) {
+                $diff[] = $elementDiff;
+            }
+        }
+
+        foreach ($diff as $i => $value) {
+            $this->assertEquals(floatval(0), $value);
+        }
+    }
 }
