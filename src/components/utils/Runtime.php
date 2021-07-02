ec51b385254fe5c6cefef11d272efa1c33bcbd18
luoxiaojun <luoxiaojun1992@sina.cn>
2021-07-03 00:27:44 +0800

    update .travis.yml

--- src/components/utils/Runtime.php
+++ src/components/utils/Runtime.php
@@ -6,6 +6,7 @@ class Runtime
 {
     public static function supportFFI()
     {
-        return version_compare(PHP_VERSION, '7.4.0') >= 0;
+        return (version_compare(PHP_VERSION, '7.4.0') >= 0) &&
+            class_exists('\FFI');
     }
 }
