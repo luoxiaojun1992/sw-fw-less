8ce0c32c4abed7f60a4f21a0b0428aca980a17ab
luoxiaojun <luoxiaojun1992@sina.cn>
2021-07-02 13:48:07 +0800

    test simd

--- src/components/utils/math/ffi/c/cbuild.sh
+++ src/components/utils/math/ffi/c/cbuild.sh
@@ -4,7 +4,7 @@ os=`uname -s`
 
 case $os in
 'Linux')
-gcc -O2 -fPIC -std=c99 -shared -mssse3 -g math.c -o linux/libcmath.so
+gcc -O2 -fPIC -std=c99 -shared -mssse3 -msse4 -g math.c -o linux/libcmath.so
 ;;
 'Darwin')
 gcc -O2 -fPIC -std=c99 -shared -g math.c -o darwin/libcmath.so
