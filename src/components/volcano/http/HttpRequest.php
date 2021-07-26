96f86a2bfe96063cd8c55d7a97878c4a711b6273
luoxiaojun <luoxiaojun1992@sina.cn>
2021-07-27 00:00:52 +0800

    feat: experimental volcano

---
+++ src/components/volcano/http/HttpRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SwFwLess\components\volcano\http;
+
+use SwFwLess\components\volcano\OperatorInterface;
+
+class HttpRequest implements OperatorInterface
+{
+    public function open()
+    {
+        // TODO: Implement open() method.
+    }
+
+    public function next()
+    {
+        // TODO: Implement next() method.
+    }
+
+    public function close()
+    {
+        // TODO: Implement close() method.
+    }
+}
