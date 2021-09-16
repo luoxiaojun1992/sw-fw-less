91cd8eaecd928862a1527c2e053b4480ab1a1e2c
luoxiaojun <luoxiaojun1992@sina.cn>
2021-09-16 23:57:49 +0800

    feat: mmap file storage

--- tests/components/storage/file/mmap/MemoryMapTest.php
+++ tests/components/storage/file/mmap/MemoryMapTest.php
@@ -29,6 +29,33 @@ class MemoryMapTest extends TestCase
         unlink($filePath);
     }
 
+    public function testAppendFile()
+    {
+        $filePath = __DIR__ . '/../../../../output/test_mmap_append.txt';
+        touch($filePath);
+        file_put_contents($filePath, 'test content');
+        $this->assertTrue(MemoryMap::appendFile(
+            $filePath,
+            ' test content'
+        ));
+
+        $testContent = 'test content test content';
+
+        $startTime = time();
+        while (($fileContent = file_get_contents($filePath)) !== $testContent) {
+            if (time() - $startTime > 5) {
+                break;
+            }
+        }
+
+        $this->assertEquals(
+            $testContent,
+            $fileContent
+        );
+
+        unlink($filePath);
+    }
+
     public function testReadFile()
     {
         $this->assertEquals(
