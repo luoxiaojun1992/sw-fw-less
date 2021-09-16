1084c94e7845eaf62aec2670db14e3e3e5851dbe
luoxiaojun <luoxiaojun1992@sina.cn>
2021-09-16 15:22:11 +0800

    feat: mmap file storage

--- src/components/storage/file/mmap/ffi/c/cbuild.sh
+++ src/components/storage/file/mmap/ffi/c/cbuild.sh
@@ -4,7 +4,7 @@ os=`uname -s`
 
 case $os in
 'Linux')
-gcc -O2 -fPIC -std=c99 -shared -g mmap.c -o linux/libcmmap.so
+gcc -O2 -fPIC -std=c99 -D_XOPEN_SOURCE=500 -shared -g mmap.c -o linux/libcmmap.so
 ;;
 'Darwin')
 gcc -O2 -fPIC -std=c99 -shared -g mmap.c -o darwin/libcmmap.so
