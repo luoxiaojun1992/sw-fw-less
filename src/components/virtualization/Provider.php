c0364267a9527af4916e2e61143028f0d487bb26
luoxiaojun <luoxiaojun1992@sina.cn>
2021-10-17 20:22:27 +0800

    feat: virtualization

--- src/components/virtualization/Provider.php
+++ src/components/virtualization/Provider.php
@@ -2,22 +2,41 @@
 
 namespace SwFwLess\components\virtualization;
 
+use SwFwLess\components\Config;
 use SwFwLess\components\provider\AppProviderContract;
 use SwFwLess\components\provider\WorkerProviderContract;
+use SwFwLess\components\swoole\Server;
 use SwFwLess\components\virtualization\resource\CGroup;
 use SwFwLess\components\virtualization\resource\Memory;
 
 class Provider implements AppProviderContract, WorkerProviderContract
 {
+    protected static function getVirtualizationConfig()
+    {
+        return Config::get('virtualization');
+    }
+
+    protected static function getVirtualizationSwitch($config = [])
+    {
+        return $config['switch'] ?? false;
+    }
+
+    protected static function getVirtualizationName($config = [])
+    {
+        return $config['name'] ?? 'sw-fw-less';
+    }
+
     public static function bootWorker()
     {
         if (CGroup::support()) {
-            //todo
-            $cGroupName = '';
-            $pid = 0;
-            $memoryLimit = 0;
-            Memory::addCGroupProcess($cGroupName, $pid);
-            Memory::limit($cGroupName, $memoryLimit);
+            $virtualizationConfig = static::getVirtualizationConfig();
+            if (static::getVirtualizationSwitch($virtualizationConfig)) {
+                $cGroupName = static::getVirtualizationName($virtualizationConfig);
+                $pid = Server::getInstance()->worker_pid;
+                $memoryLimit = $virtualizationConfig['memory_limit'] ?? 10240000;
+                Memory::addCGroupProcess($cGroupName, $pid);
+                Memory::limit($cGroupName, $memoryLimit);
+            }
         }
     }
 
@@ -29,11 +48,14 @@ class Provider implements AppProviderContract, WorkerProviderContract
     public static function bootApp()
     {
         if (CGroup::support()) {
-            $cGroupName = '';
-            if (Memory::subCGroupExists($cGroupName)) {
-                Memory::delCGroup($cGroupName);
-            } else {
-                Memory::createCGroup($cGroupName);
+            $virtualizationConfig = static::getVirtualizationConfig();
+            if (static::getVirtualizationSwitch($virtualizationConfig)) {
+                $cGroupName = static::getVirtualizationName($virtualizationConfig);
+                if (Memory::subCGroupExists($cGroupName)) {
+                    Memory::delCGroup($cGroupName);
+                } else {
+                    Memory::createCGroup($cGroupName);
+                }
             }
         }
     }
@@ -41,9 +63,12 @@ class Provider implements AppProviderContract, WorkerProviderContract
     public static function shutdownApp()
     {
         if (CGroup::support()) {
-            $cGroupName = '';
-            if (Memory::subCGroupExists($cGroupName)) {
-                Memory::delCGroup($cGroupName);
+            $virtualizationConfig = static::getVirtualizationConfig();
+            if (static::getVirtualizationSwitch($virtualizationConfig)) {
+                $cGroupName = static::getVirtualizationName($virtualizationConfig);
+                if (Memory::subCGroupExists($cGroupName)) {
+                    Memory::delCGroup($cGroupName);
+                }
             }
         }
     }
