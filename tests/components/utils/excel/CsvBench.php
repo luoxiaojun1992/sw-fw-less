39454bbc5c63e9721df423d91f8fe92674d708f5
luoxiaojun <luoxiaojun1992@sina.cn>
2021-12-22 13:21:54 +0800

    test: csv util benchmark

--- tests/components/utils/excel/CsvBench.php
+++ tests/components/utils/excel/CsvBench.php
@@ -4,6 +4,29 @@ namespace SwFwLessTests\components\utils\excel;
 
 class CsvBench
 {
+    public function benchNativeCsv()
+    {
+        $filePath = __DIR__ . '/../../../output/test_native_csv.csv';
+        if (file_exists($filePath)) {
+            unlink($filePath);
+        }
+        touch($filePath);
+
+        $csvFile = fopen($filePath, 'w+');
+
+        mt_srand(time());
+
+        for ($row = 0; $row < 100000; ++$row) {
+            $rowData = [];
+            for ($col = 0; $col < 10; ++$col) {
+                $rowData[] = mt_rand(10000, 99999);
+            }
+            fputcsv($csvFile, $rowData);
+        }
+
+        unlink($filePath);
+    }
+
     public function benchCsvUtil()
     {
         $filePath = __DIR__ . '/../../../output/test_csv_util.csv';
@@ -25,29 +48,6 @@ class CsvBench
                 $rowData[] = mt_rand(10000, 99999);
             }
             $csvFile->putCsv($rowData);
-        }
-
-        unlink($filePath);
-    }
-
-    public function benchNativeCsv()
-    {
-        $filePath = __DIR__ . '/../../../output/test_native_csv.csv';
-        if (file_exists($filePath)) {
-            unlink($filePath);
-        }
-        touch($filePath);
-
-        $csvFile = fopen($filePath, 'w+');
-
-        mt_srand(time());
-
-        for ($row = 0; $row < 100000; ++$row) {
-            $rowData = [];
-            for ($col = 0; $col < 10; ++$col) {
-                $rowData[] = mt_rand(10000, 99999);
-            }
-            fputcsv($csvFile, $rowData);
         }
 
         unlink($filePath);
