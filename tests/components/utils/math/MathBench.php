3e7d6d6855fba91d4a6b825152fc214a98c57322
luoxiaojun <luoxiaojun1992@sina.cn>
2021-12-25 22:02:14 +0800

    test: math util benchmark

--- tests/components/utils/math/MathBench.php
+++ tests/components/utils/math/MathBench.php
@@ -30,4 +30,37 @@ class MathBench
 
         $mathUtil->sum(null, 1000000, $arr);
     }
+
+    public function benchNativeVectorAdd()
+    {
+        mt_srand(time());
+
+        for ($i = 0; $i < 100000; ++$i) {
+            mt_rand(10000, 99999) + mt_rand(10000, 99999);
+        }
+    }
+
+    public function benchSimdVectorAdd()
+    {
+        $mathUtil = Math::create();
+
+        $vector1 = $mathUtil->createCFloatNumbers(4);
+        $vector2 = $mathUtil->createCFloatNumbers(4);
+
+        mt_srand(time());
+
+        for ($i = 0; $i < (100000 / 4); ++$i) {
+            $vector1[0] = mt_rand(10000, 99999);
+            $vector1[1] = mt_rand(10000, 99999);
+            $vector1[2] = mt_rand(10000, 99999);
+            $vector1[3] = mt_rand(10000, 99999);
+
+            $vector2[0] = mt_rand(10000, 99999);
+            $vector2[1] = mt_rand(10000, 99999);
+            $vector2[2] = mt_rand(10000, 99999);
+            $vector2[3] = mt_rand(10000, 99999);
+
+            $mathUtil->vectorAdd($vector1, $vector2, 4);
+        }
+    }
 }
